ed7f914b898156d26a97ede6d623d3ec8506e7b0
dhruvhacks <sridhruv@yahoo.com>
2018-09-03 05:42:21 +0530

    Merged model

--- ffa.py
+++ ffa.py
@@ -1,69 +1,127 @@
 import numpy as np
 import cost_functions
 import plot_graph
+from model import Model
+from dataset_util import read_data_sets
 
-class ffa():
+class ffa(object):
     '''
     Ffirefly algorithm.
     '''
-    def __init__(self,rnges,population,alpha=0.2,gamma=1.0):
+    def __init__(self,rnges,input_nodes,logits,population,alpha=0.2,gamma=1.0):
         self.population = population
         self.alpha = 0.5
         self.gamma = 1.0
         self.rnges = rnges
         self.xn = np.zeros(0)
-        self.xy = np.zeros(0)
+        self.yn = np.zeros(0)
+        self.zn = list()
+        self.fireflies = list()
+        self.input_nodes = [input_nodes]
+        self.logits = logits
         self.lightn = np.zeros(0)
+        self.data = read_data_sets("MNIST_data/", one_hot=True)
 
+    def build_nodes(self):
+        print("Building Nodes")
+        for i in self.yn:
+            self.zn.append(np.random.randint(low=self.rnges['node_count_lower'],high=self.rnges['node_count_upper'],size=self.rnges['h_layers_count_upper']))
+
+    def collect_fireflies(self):
+        print("Collecting fireflies")
+        self.fireflies = list()
+        for i in range(len(self.xn)):
+            self.fireflies.append(Model(self.xn[i], self.yn[i], self.input_nodes[0], self.logits, self.zn[i], self.data))
+    
     def initiate(self,max_gen):
-        x_range = self.rnges['xupper']-self.rnges['xlower']
-        y_range = self.rnges['yupper']-self.rnges['ylower']
-        self.xn = np.random.rand(self.population)*x_range+self.rnges['xlower']
-        self.yn = np.random.rand(self.population)*y_range+self.rnges['ylower']
+        print("Called ffa initiate")
+        learning_rate_range = self.rnges['learning_rate_upper']-self.rnges['learning_rate_lower']
+        self.xn = np.random.rand(self.population)*learning_rate_range+self.rnges['learning_rate_lower']
+        self.yn = np.random.randint(low=self.rnges['h_layers_count_lower']+2, high=self.rnges['h_layers_count_upper'],size=self.population)
+        self.build_nodes()
         self.lightn = np.zeros(self.yn.shape)
+        self.collect_fireflies()
 
     def findrange(self):
         for i in range(self.yn.size):
-            if self.xn[i]<=self.rnges['xlower']:
-                self.xn[i] = self.rnges['xlower']
-            if self.xn[i]>=self.rnges['xupper']:
-                self.xn[i] = self.rnges['xupper']
-            if self.yn[i]<=self.rnges['ylower']:
-                self.yn[i] = self.rnges['ylower']
-            if self.yn[i]>=self.rnges['yupper']:
-                self.yn[i] = self.rnges['yupper']
+            if self.xn[i]<=self.rnges['learning_rate_lower']:
+                self.xn[i] = self.rnges['learning_rate_lower']
+            if self.xn[i]>=self.rnges['learning_rate_upper']:
+                self.xn[i] = self.rnges['learning_rate_upper']
+            if self.yn[i]<=self.rnges['h_layers_count_lower']:
+                self.yn[i] = self.rnges['h_layers_count_lower']
+            if self.yn[i]>=self.rnges['h_layers_count_upper']:
+                self.yn[i] = self.rnges['h_layers_count_upper']
+            for i in range(len(self.zn)):
+                for j in range(len(self.zn[i])):
+                    if self.zn[i][j]>=self.rnges['node_count_upper']:
+                        self.zn[i][j] = self.rnges['node_count_upper']
+                    if self.zn[i][j]<=self.rnges['node_count_lower']:
+                        self.zn[i][j] = self.rnges['node_count_lower']
 
-    def ffa_move(self,xo,yo,lighto):
+    def ffa_move(self,xo,yo,zo,lighto):
         ni = self.yn.shape[0]
         nj = yo.shape[0]
+        temp = 0
         for i in range(ni):
             for j in range(nj):
-                r = np.sqrt((self.xn[i]-xo[j])**2 + (self.yn[i]-yo[j])**2)
+                original_layers_count = self.yn[i]-2
+                r1 = np.sqrt((self.xn[i]-xo[j])**2 + (self.yn[i]-yo[j])**2)
+                if self.yn[i]<yo[j]:
+                    for k in range(self.yn[i]):
+                        temp+=(self.zn[i][k]-zo[j][k])**2
+                else:
+                    for k in range(yo[i]):
+                        temp+=(self.zn[i][k]-zo[j][k])**2
+                r2 = np.sqrt(temp)
                 if self.lightn[i]<lighto[j]:
                     beta0 = 1
-                    beta = beta0*np.exp(-1*self.gamma*r**2)
-                    self.xn[i] = self.xn[i]*(1-beta)+xo[j]*beta+self.alpha*(np.random.rand()-0.5)
-                    self.yn[i] = self.yn[i]*(1-beta)+yo[j]*beta+self.alpha*(np.random.rand()-0.5)
+                    beta1 = beta0*np.exp(-1*self.gamma*r1**2)
+                    beta2 = beta0*np.exp(-1*self.gamma*r2**2)
+                    self.xn[i] = self.xn[i]*(1-beta1)+xo[j]*beta1+self.alpha*(np.random.rand()-0.5)
+                    self.yn[i] = self.yn[i]*(1-beta1)+yo[j]*beta1+self.alpha*(np.random.randint(low=self.rnges['h_layers_count_lower'], high=self.rnges['h_layers_count_upper'])-10)
+                    for k in range(self.yn[i]):
+                        if k < original_layers_count:
+                            if k<yo[j]:
+                                self.zn[i][k] = self.zn[i][k]*(1-beta2)+zo[j][k]*beta2+self.alpha*(np.random.randint(low=self.rnges['node_count_lower'], high=self.rnges['node_count_upper'])-0.5)
+                        elif k < yo[j]:
+                            self.zn[i][k] = zo[j][k]
+        print("INSIDE MOVE!")
+        print(self.xn,self.yn)
         self.findrange()
+        self.collect_fireflies()
+        print("again insode MOVWE")
+        print(self.xn,self.yn)
+        print(self.fireflies)
 
     def firefly_simple(self,max_gen):
-        z = cost_functions.landmark(self.rnges, mode=1)[2]
         self.initiate(max_gen)
-        plot_graph.plot_case(self.xn,self.yn)
+        print("Inside ffa_simple")
         for i in range(max_gen):
-            values = {'x':self.xn, 'y':self.yn}
-            zn = cost_functions.landmark(values, mode=0)[0]
-            lighto = np.sort(zn)
-            indexes = np.argsort(zn)
+            print(self.xn)
+            print(self.yn)
+            print(self.zn)
+            qn = []
+            for j in range(len(self.xn)):
+                qn.append(self.fireflies[j].make_layer())
+            lighto = np.sort(qn)
+            indexes = np.argsort(qn)
             # lighto = lighto[::-1]         # for minima
             # indexes = indexes[::-1]       # for minima
-            xo = np.array([self.xn[i] for i in indexes])
-            yo = np.array([self.yn[i] for i in indexes])
-            self.ffa_move(xo,yo,lighto)
-        plot_graph.plot_case(self.xn,self.yn)
+            print("\n At step "+str(i)+"with values- ", qn)
+            xo = np.array([self.xn[j] for j in indexes])
+            yo = np.array([self.yn[j] for j in indexes])
+            zo = list(np.array([self.zn[j] for j in indexes]))
+            print(self.xn,xo)
+            print(self.yn,yo)
+            print(self.zn,zo)
+            # print(type(zo),type(self.zn))
+            self.ffa_move(xo,yo,zo,lighto)
+            print("\n\n Moved "+str(i))
 
 
 if __name__ == '__main__':
-    rnges = {'xlower':-5, 'xupper':5, 'ylower':-5, 'yupper':5}
-    obj = ffa(rnges=rnges, population=20)
-    obj.firefly_simple(200)
+    rnges = {'learning_rate_lower':0.0001, 'learning_rate_upper':0.1, 'h_layers_count_upper':20, 'h_layers_count_lower':3, 'node_count_lower':10, 'node_count_upper':150}
+    # rnges = {'xlower':-5, 'xupper':5, 'ylower':-5, 'yupper':5}
+    obj = ffa(rnges=rnges, input_nodes=784, logits=10, population=5)
+    obj.firefly_simple(max_gen=20)
